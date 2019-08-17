55864142f3a6c9db83364003df2b97ceec860f00
Hristina Dakic <hristina.dakic@gmail.com>
2019-08-17 17:21:54 +0200

    dadd date created

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ var locations = [];
 // Create class for locations 
 //==================================================
 class Location {
-	constructor (name, city, zip, adress, image) {
+	constructor (created, name, city, zip, adress, image) {
+		this.created = new Date();
 		this.name = name;
 		this.city = city;
 		this.zip = zip;
@@ -28,6 +29,7 @@ class Location {
 		return `
 			<p class="text-dark font-weight-bold">${this.name}</p>
 			<img src="img/${this.image}" alt="image" class="img-thumbnail d-none d-md-block">
+			<p class="font-weight-lighter small font-italic">Created: ${this.created.toUTCString()}</p>
 			<p class="mt-3 mb-0">${this.adress + ", " + this.zip + " " + this.city}</p>
 			`;
 	};
@@ -35,10 +37,10 @@ class Location {
 }
 
 
-new Location ("St. Charles Church ", "Vienna", "1010", "Karlsplatz 1", "charles.JPG");
-new Location ("KunstHausWien", "Vienna", "1030", "Untere Weißgerberstraße 13", "hundertwasser.JPG");
-new Location ("Stadtpark City Park", "Vienna", "1010", "Parkring 1", "stadtpark.JPG");
-new Location ("Schönbrunn Palace", "Vienna", "1130", "Schönbrunner Schloßstraße 47", "schönbrunn.JPG");
+new Location ("", "St. Charles Church", "Vienna", "1010", "Karlsplatz 1", "charles.JPG");
+new Location ("", "KunstHausWien", "Vienna", "1030", "Untere Weißgerberstraße 13", "hundertwasser.JPG");
+new Location ("", "Stadtpark City Park", "Vienna", "1010", "Parkring 1", "stadtpark.JPG");
+new Location ("", "Schönbrunn Palace", "Vienna", "1130", "Schönbrunner Schloßstraße 47", "schönbrunn.JPG");
 
 for (i=0; i<locations.length; i++) {
 	if (locations[i].constructor.name == "Location") {
@@ -50,8 +52,8 @@ for (i=0; i<locations.length; i++) {
 //==================================================
 
 class Restaurant extends Location {
-	constructor(name, city, zip, adress, image, tel, type, web) {
-		super(name, city, zip, adress, image);
+	constructor(created, name, city, zip, adress, image, tel, type, web) {
+		super(created, name, city, zip, adress, image);
 		this.tel = tel;
 		this.type = type;
 		this.web = web;
@@ -68,8 +70,8 @@ class Restaurant extends Location {
 
 }
 
-new Restaurant ("ALL REIS", "Vienna", "1150", "Schweglerstraße 12", "allreis.JPG", "+431 7864668", "Thai", "www.allreis.com");
-new Restaurant ("Cafe Ansari", "Vienna", "1020", "Praterstraße 15", "ansari.JPG", "+431 2765102", "Georgian", "www.cafeansari.at");
+new Restaurant ("", "ALL REIS", "Vienna", "1150", "Schweglerstraße 12", "allreis.JPG", "+431 7864668", "Thai", "www.allreis.com");
+new Restaurant ("", "Cafe Ansari", "Vienna", "1020", "Praterstraße 15", "ansari.JPG", "+431 2765102", "Georgian", "www.cafeansari.at");
 
 for (i=0; i<locations.length; i++) {
 	if (locations[i].constructor.name == "Restaurant") {
@@ -81,8 +83,8 @@ for (i=0; i<locations.length; i++) {
 //==========================================
 
 class Event extends Location {
-	constructor(name, city, zip, adress, image, web, date, time, price) {
-		super(name, city, zip, adress, image);
+	constructor(created, name, city, zip, adress, image, web, date, time, price) {
+		super(created, name, city, zip, adress, image);
 		this.web = web;
 		this.date = date;
 		this.time = time;
@@ -101,9 +103,9 @@ class Event extends Location {
 }
 
 
-new Event ("Sukhishvili - Georgisches Nationalballett", "Vienna", "1150", "Wiener Stadthalle, Hall F, Roland Rainer Platz 1", "sukhi.jpeg", "www.stadthalle.com", "18.11.2019", "20:00", "from 63,70 EUR");
-new Event ("Wiener Stadtfest Aftershow Party", "Vienna", "1150", "Wiener Stadthalle, Hall F, Roland Rainer Platz 1", "fest.jpeg", "www.stadthalle.com", "31.08.2019", "18:00", "Free" );
-new Event ("Max Raabe & Palast Orchester", "Vienna", "1150", "Wiener Stadthalle, Hall F, Roland Rainer Platz 1", "max.jpeg", "www.stadthalle.com", "08.05.2020", "20:00", "from 63,00 EUR" );
+new Event ("", "Sukhishvili - Georgisches Nationalballett", "Vienna", "1150", "Wiener Stadthalle, Hall F, Roland Rainer Platz 1", "sukhi.jpeg", "www.stadthalle.com", "18.11.2019", "20:00", "from 63,70 EUR");
+new Event ("", "Wiener Stadtfest Aftershow Party", "Vienna", "1150", "Wiener Stadthalle, Hall F, Roland Rainer Platz 1", "fest.jpeg", "www.stadthalle.com", "31.08.2019", "18:00", "Free" );
+new Event ("", "Max Raabe & Palast Orchester", "Vienna", "1150", "Wiener Stadthalle, Hall F, Roland Rainer Platz 1", "max.jpeg", "www.stadthalle.com", "08.05.2020", "20:00", "from 63,00 EUR" );
 
 for (i=0; i<locations.length; i++) {
 	if (locations[i].constructor.name == "Event") {
