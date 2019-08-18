5b84ffb9c3da1b4a8ced1b69ccd884db2f0b6089
Hristina Dakic <hristina.dakic@gmail.com>
2019-08-18 22:40:31 +0200

    adds sorting of elements

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 var locations = [];
 
-
-// Create class for locations 
+//============================================
+// Create the superior class for locations 
 //==================================================
 class Location {
 	constructor (created, name, city, zip, adress, image) {
@@ -36,18 +36,7 @@ class Location {
 
 }
 
-
-new Location (new Date(2018, 8, 17, 10, 33), "St. Charles Church", "Vienna", "1010", "Karlsplatz 1", "charles.JPG");
-new Location (new Date(2019, 4, 17, 12, 20), "KunstHausWien", "Vienna", "1030", "Untere Weißgerberstraße 13", "hundertwasser.JPG");
-new Location (new Date(2017, 2, 17, 14, 40), "Stadtpark City Park", "Vienna", "1010", "Parkring 1", "stadtpark.JPG");
-new Location (new Date(2019, 6, 17, 15, 50), "Schönbrunn Palace", "Vienna", "1130", "Schönbrunner Schloßstraße 47", "schönbrunn.JPG");
-
-for (i=0; i<locations.length; i++) {
-	if (locations[i].constructor.name == "Location") {
-		locations[i].display("#places");
-	}
-}
-
+//============================================
 // Create class for restaurants 
 //==================================================
 
@@ -67,18 +56,9 @@ class Restaurant extends Location {
 			<a href="${this.web}">${this.web}</a>
 		`
 	};
-
 }
 
-new Restaurant (new Date(2018, 7, 15, 9, 30), "ALL REIS", "Vienna", "1150", "Schweglerstraße 12", "allreis.JPG", "+431 7864668", "Thai", "www.allreis.com");
-new Restaurant (new Date(2018, 10, 17, 10, 33), "Cafe Ansari", "Vienna", "1020", "Praterstraße 15", "ansari.JPG", "+431 2765102", "Georgian", "www.cafeansari.at");
-
-for (i=0; i<locations.length; i++) {
-	if (locations[i].constructor.name == "Restaurant") {
-		locations[i].display("#restaurants");
-	}
-}
-
+//============================================
 // Create class for events 
 //==========================================
 
@@ -101,19 +81,86 @@ class Event extends Location {
 	};
 
 }
+//============================================
+//create objects
+//============================================
 
 
-new Event (new Date(2018, 5, 17, 10, 33), "Sukhishvili - Georgisches Nationalballett", "Vienna", "1150", "Wiener Stadthalle, Hall F, Roland Rainer Platz 1", "sukhi.jpeg", "www.stadthalle.com", "18.11.2019", "20:00", "from 63,70 EUR");
+new Location (new Date(2018, 8, 17, 10, 33), "St. Charles Church", "Vienna", "1010", "Karlsplatz 1", "charles.JPG");
+new Location (new Date(2019, 4, 17, 12, 20), "KunstHausWien", "Vienna", "1030", "Untere Weißgerberstraße 13", "hundertwasser.JPG");
+new Location (new Date(2017, 2, 17, 14, 40), "Stadtpark City Park", "Vienna", "1010", "Parkring 1", "stadtpark.JPG");
+new Location (new Date(2019, 6, 17, 15, 50), "Schönbrunn Palace", "Vienna", "1130", "Schönbrunner Schloßstraße 47", "schönbrunn.JPG");
+
+new Restaurant (new Date(2018, 7, 15, 9, 30), "ALL REIS", "Vienna", "1150", "Schweglerstraße 12", "allreis.JPG", "+431 7864668", "Thai", "www.allreis.com");
+new Restaurant (new Date(2018, 10, 17, 10, 33), "Cafe Ansari", "Vienna", "1020", "Praterstraße 15", "ansari.JPG", "+431 2765102", "Georgian", "www.cafeansari.at");
+
+new Event (new Date(2019, 5, 17, 10, 33), "Sukhishvili - Georgisches Nationalballett", "Vienna", "1150", "Wiener Stadthalle, Hall F, Roland Rainer Platz 1", "sukhi.jpeg", "www.stadthalle.com", "18.11.2019", "20:00", "from 63,70 EUR");
 new Event (new Date(2018, 6, 17, 10, 33), "Wiener Stadtfest Aftershow Party", "Vienna", "1150", "Wiener Stadthalle, Hall F, Roland Rainer Platz 1", "fest.jpeg", "www.stadthalle.com", "31.08.2019", "18:00", "Free" );
-new Event (new Date(2019, 3, 17, 10, 33), "Max Raabe & Palast Orchester", "Vienna", "1150", "Wiener Stadthalle, Hall F, Roland Rainer Platz 1", "max.jpeg", "www.stadthalle.com", "08.05.2020", "20:00", "from 63,00 EUR" );
+new Event (new Date(2018, 3, 17, 10, 33), "Max Raabe & Palast Orchester", "Vienna", "1150", "Wiener Stadthalle, Hall F, Roland Rainer Platz 1", "max.jpeg", "www.stadthalle.com", "08.05.2020", "20:00", "from 63,00 EUR" );
+
+//============================================
+//render all places, restaurants and events
+//============================================
 
 for (i=0; i<locations.length; i++) {
-	if (locations[i].constructor.name == "Event") {
+	if (locations[i].constructor.name == "Location") {
+		locations[i].display("#places");
+	}
+	else if (locations[i].constructor.name == "Restaurant") {
+		locations[i].display("#restaurants");
+	}
+	else if (locations[i].constructor.name == "Event") {
 		locations[i].display("#events");
 	}
 }
 
+//============================================
+//on click render the places, restaurants and events in the ascending order
+//============================================
 
+$("#ascending").click(function() {
+	locations.sort(function(a, b){
+		return a.created.getTime()-b.created.getTime();
+	});
+
+	$("#places").html("");
+	$("#restaurants").html("");
+	$("#events").html("");
+
+	for (i=0; i<locations.length; i++) {
+		if (locations[i].constructor.name == "Location") {
+			locations[i].display("#places");
+		}
+		else if (locations[i].constructor.name == "Restaurant") {
+			locations[i].display("#restaurants");
+		}
+		else if (locations[i].constructor.name == "Event") {
+			locations[i].display("#events");
+		}
+	}
+});
+
+$("#descending").click(function() {
+	locations.sort(function(a, b){
+		return b.created.getTime() - a.created.getTime();
+	});
+
+	$("#places").html("");
+	$("#restaurants").html("");
+	$("#events").html("");
+
+	for (i=0; i<locations.length; i++) {
+		if (locations[i].constructor.name == "Location") {
+			locations[i].display("#places");
+		}
+		else if (locations[i].constructor.name == "Restaurant") {
+			locations[i].display("#restaurants");
+		}
+		else if (locations[i].constructor.name == "Event") {
+			locations[i].display("#events");
+		}
+	}
+});
 
 
 
